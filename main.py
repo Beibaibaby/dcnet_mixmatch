Supported mixed precision training. Changes: fix casting for interpolation. Also, use BCEWithLogits for gates since BCELoss was not deemed suitable for mp

main.py
@@ -52,57 +52,10 @@ def exec(cfg: DictConfig) -> None:
                                 limit_val_batches=cfg.trainer.limit_val_batches,
                                 limit_test_batches=cfg.trainer.limit_test_batches)
         pl_trainer.test(trainer, [loader])
-
     elif cfg.task.name == 'analyze_segmentation':
         from analysis.analyze_segmentation import main_calc_segmentation_metrics
         main_calc_segmentation_metrics(config=cfg,
                                        data_loader=dataloader_factory.build_dataloader_for_split(cfg, cfg.data_split))
-    # elif cfg.task.name == 'visualize_cams':
-    #     if cfg.load_checkpoint is None:
-    #         cfg.load_checkpoint = os.path.join(os.getcwd(), f'ckpt_latest.pt')
-    #     from trainers.visualize_cams import CAMVisualizer
-    #     visualizer = CAMVisualizer(cfg)
-    #     visualizer.visualize(build_dataloader_for_split(cfg, cfg.task.split), cfg.task.item_ixs)
-    # elif cfg.task.name == 'confusion_matrix':
-    #     if cfg.load_checkpoint is None:
-    #         cfg.load_checkpoint = os.path.join(os.getcwd(), f'ckpt_latest.pt')
-    #     from trainers.confusion_matrix import ConfusionMatrixVisualizer
-    #     visualizer = ConfusionMatrixVisualizer(cfg)
-    #     visualizer.save_confusion_matrix(data_loaders['Test']['Val'])
-    # elif cfg.task.name in ['self_consistency_analysis']:
-    #     from analysis.self_consistency import SelfConsistencyAnalysis
-    #     if cfg.load_checkpoint is None:
-    #         cfg.load_checkpoint = os.path.join(cfg.full_expt_dir, f'ckpt_early_stopping.pt')
-    #     runner = eval(cfg.task.runner)(cfg)
-    #     runner.run(data_loaders['Test'])
-    # elif cfg.task.name in ['calibrate', 'calibrate_exit_gates']:
-    #     main_exec = trainer_factory.build_trainer(cfg)  # Has useful functions for the calibration script
-    #     logging.getLogger().info("Config:")
-    #     logging.getLogger().info(OmegaConf.to_yaml(cfg, sort_keys=True, resolve=True))
-    #
-    #     if cfg.task.name == 'calibrate':
-    #         from trainers.calibrator import Calibrator
-    #         logging.getLogger().info(f"Calibrating using logits...")
-    #         calibrator = Calibrator(cfg, main_exec=main_exec)
-    #     elif cfg.task.name == 'calibrate_exit_gates':
-    #         from trainers.exit_gate_calibrator import ExitGateCalibrator
-    #         logging.getLogger().info(f"Calibrating using exit gates...")
-    #         calibrator = ExitGateCalibrator(cfg, main_exec=main_exec)
-    #     calibrator.calibrate(build_dataloader_for_split(cfg, cfg.data_split))
-    # elif cfg.task.name == 'test_exit_strategies':
-    #     if cfg.load_checkpoint is None:
-    #         cfg.load_checkpoint = os.path.join(os.getcwd(), f'ckpt_latest.pt')
-    #
-    #     from trainers.exit_strategy_tester import ExitStrategyTester
-    #     main_exec = ExitStrategyTester(cfg)
-    #     main_exec.test_exit_strategies(data_loaders)
-    # elif cfg.task.name == 'data_driven_init':
-    #     from models.model_factory import build_model
-    #     from projection.data_driven_init import data_driven_initialization
-    #     device = torch.device('cpu')
-    #     model = build_model(cfg, device)
-    #     data_loader = data_loaders['Train']
-    #     data_driven_initialization(model, data_loader, cfg.task.save_dir)
     else:
         data_loaders = dataloader_factory.build_dataloaders(cfg)
         trainer = trainer_factory.build_trainer(cfg)
@@ -117,26 +70,12 @@ def exec(cfg: DictConfig) -> None:
                                 num_sanity_val_steps=0,
                                 limit_train_batches=cfg.trainer.limit_train_batches,
                                 limit_val_batches=cfg.trainer.limit_val_batches,
-                                limit_test_batches=cfg.trainer.limit_test_batches
-                                )
-
+                                limit_test_batches=cfg.trainer.limit_test_batches,
+                                precision=cfg.trainer.precision)
         pl_trainer.fit(trainer,
                        train_dataloaders=data_loaders['train'],
                        val_dataloaders=list(data_loaders['val'].values()))
         pl_trainer.test(trainer, list(data_loaders['test'].values()))
-
-    #
-    # elif cfg.task.name == 'compute_intrinsic_measures':
-    #     if cfg.load_checkpoint is None:
-    #         cfg.load_checkpoint = os.path.join(cfg.full_expt_dir, f'ckpt_latest.pt')
-    #
-    #     trainer = trainer_factory.build_trainer(cfg)
-    #     logging.getLogger().info("Config:")
-    #     logging.getLogger().info(OmegaConf.to_yaml(cfg, sort_keys=True, resolve=True))
-    #
-    #     new_test_loaders = {cfg.data_split: data_loaders['Test'][cfg.data_split]}
-    #     trainer.load_checkpoint_and_test_all(epoch=-1, test_loaders=new_test_loaders, force_save=False)
-    #     compute_intrinsic_measures(trainer.model, trainer=trainer, dataloaders=new_test_loaders)
 
 
 ROOT = '/hdd/robik'

models/occam_lib.py
@@ -65,8 +65,8 @@ class SimpleGate(nn.Module):
         if len(x.shape) == 1:
             x = x.unsqueeze(0)
 
-        x = self.net(x)
-        x = torch.sigmoid(x).squeeze()
+        x = self.net(x).squeeze()
+        # x = torch.sigmoid(x).squeeze()
         return x
 
 
@@ -84,7 +84,7 @@ def get_early_exit_ixs(exit_ix_to_exit_probas):
 
     for exit_ix in exit_ix_to_exit_probas:
         exit_probas = exit_ix_to_exit_probas[exit_ix]
-        use_next_exit = (exit_probas < 0.5).int()
+        use_next_exit = (torch.sigmoid(exit_probas) < 0.5).int()
         early_exit_ixs = torch.where(((1 - use_next_exit) * (1 - has_exited)).bool(),
                                      torch.ones_like(exit_ix_to_exit_probas[0]) * exit_ix,
                                      early_exit_ixs)

trainers/occam_trainer.py
@@ -218,6 +218,7 @@ class ExitGateLoss():
             _gate_loss_wts = torch.where(gate_gt > 0,
                                          (torch.ones_like(gate_gt) / (_exit_cnt + eps)) ** self.balance_factor,
                                          (torch.ones_like(gate_gt) / (_continue_cnt + eps)) ** self.balance_factor)
-            gate_loss = _gate_loss_wts * F.binary_cross_entropy(gates, gate_gt.float(), reduction='none')
+            # gate_loss = _gate_loss_wts * F.binary_cross_entropy(gates, gate_gt.float(), reduction='none')
+            gate_loss = _gate_loss_wts * F.binary_cross_entropy_with_logits(gates, gate_gt.float(), reduction='none')
             return gate_loss.mean()
         return torch.zeros_like(logits.max(dim=1)[0])

utils/cam_utils.py
@@ -73,7 +73,14 @@ def interpolate(x, h, w):
         x = x.unsqueeze(1)
     if x.shape[2] == h and x.shape[3] == w:
         return x
-    return F.interpolate(x, (h, w), mode='bilinear', align_corners=False).squeeze()
+    to_half = False
+    if isinstance(x, torch.HalfTensor):
+        to_half = True
+        x = x.float()
+    x = F.interpolate(x, (h, w), mode='bilinear', align_corners=False).squeeze()
+    if to_half:
+        x = x.half()
+    return x
 
 
 def get_early_exit_cams(model_out, H=None, W=None):

utils/metrics.py
@@ -105,7 +105,8 @@ class GateMetric():
         self.exited = 0
         self.threshold = threshold
 
-    def update(self, exit_probas):
+    def update(self, exit_logits):
+        exit_probas = torch.sigmoid(exit_logits)
         for proba in exit_probas:
             if proba >= self.threshold:
                 self.exited += 1