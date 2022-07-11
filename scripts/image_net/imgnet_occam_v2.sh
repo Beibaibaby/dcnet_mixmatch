983f6a3f2610cefabb340c9767870c44e0e37784
erobic <robikshrestha@gmail.com>
2022-07-10 21:50:22 -0400

    Search for MDCA loss weight

--- scripts/image_net/imgnet_occam_v2.sh
+++ scripts/image_net/imgnet_occam_v2.sh
@@ -8,8 +8,8 @@ optim=image_net
 subset_percent=16
 precision=16
 
-for calibration_loss_wt in 0; do
-  for model in occam_resnet18_v2_k9753 occam_resnet18_v2_k3 occam_resnet18_v2_k9753_same_width; do
+for calibration_loss_wt in 1 2 5; do
+  for model in occam_resnet18_v2_k9753_same_width; do
     for main_loss in CELoss; do
       for calibration_loss in MDCALoss; do
         CUDA_VISIBLE_DEVICES=${GPU} python main.py \
