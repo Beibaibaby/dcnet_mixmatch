d4daf804e98af514585cae6319aca0e3f902a308
erobic <robikshrestha@gmail.com>
2022-07-17 18:59:56 -0400

    Focal Loss

--- scripts/image_net/imgnet_focal_loss_calibration.sh
+++ scripts/image_net/imgnet_focal_loss_calibration.sh
@@ -13,24 +13,26 @@ for calibration_loss_wt in 1; do
     for detach_prev in True; do
       for model in occam_resnet18_v2_k9753; do
         for main_loss in MultiExitFocalLoss; do
-          CUDA_VISIBLE_DEVICES=${GPU} python main.py \
-          model.name=${model} \
-          trainer=occam_trainer_v2 \
-          trainer.precision=${precision} \
-          trainer.main_loss=${main_loss} \
-          trainer.gamma=${gamma} \
-          trainer.detach_prev=${detach_prev} \
-          trainer.calibration_loss_wt=${calibration_loss_wt} \
-          dataset=${dataset} \
-          dataset.subset_percent=${subset_percent} \
-          optimizer=${optim} \
-          expt_suffix=${main_loss}_gamma_${gamma}_detach_prev_${detach_prev}_cal_${calibration_loss_wt}_subset_${subset_percent}_prec_${precision}
+          for calibration_loss in MDCALoss; do
+            CUDA_VISIBLE_DEVICES=${GPU} python main.py \
+            model.name=${model} \
+            trainer=occam_trainer_v2 \
+            trainer.precision=${precision} \
+            trainer.main_loss=${main_loss} \
+            trainer.gamma=${gamma} \
+            trainer.detach_prev=${detach_prev} \
+            trainer.calibration_loss=${calibration_loss} \
+            trainer.calibration_loss_wt=${calibration_loss_wt} \
+            dataset=${dataset} \
+            dataset.subset_percent=${subset_percent} \
+            optimizer=${optim} \
+            expt_suffix=${main_loss}_gamma_${gamma}_detach_prev_${detach_prev}_${calibration_loss}_${calibration_loss_wt}_subset_${subset_percent}_prec_${precision}
+          done
         done
       done
     done
   done
 done
-
 #  \
 #          trainer.limit_train_batches=2 \
 #          trainer.limit_val_batches=2 \
