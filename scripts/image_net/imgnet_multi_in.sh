18dce60f157c250379e03578ffef61731c8bd170
erobic <robikshrestha@gmail.com>
2022-07-22 22:41:15 -0400

    variants

--- scripts/image_net/imgnet_multi_in.sh
+++ scripts/image_net/imgnet_multi_in.sh
@@ -8,8 +8,8 @@ optim=image_net
 subset_percent=16
 precision=16
 
-for model in occam_resnet18_v2_in_9; do
-  for blur_sigma in 0.1 1.0 0.5; do # 2.0 1.0 0.5
+for model in occam_resnet18_v2_k9753; do
+  for blur_sigma in 2 1 0.5; do
     for main_loss in CELoss; do
       for calibration_loss in MDCALoss; do
         for calibration_loss_wt in 0; do
@@ -19,25 +19,22 @@ for model in occam_resnet18_v2_in_9; do
           trainer.precision=${precision} \
           trainer.main_loss=${main_loss} \
           trainer.blur_sigma=${blur_sigma} \
+          trainer.input_views=['edge'] \
           trainer.calibration_loss_wt=${calibration_loss_wt} \
           trainer.calibration_loss_wt=${calibration_loss_wt} \
           dataset=${dataset} \
           dataset.subset_percent=${subset_percent} \
           optimizer=${optim} \
-          expt_suffix=${main_loss}_blur_sigma_${blur_sigma}_${calibration_loss}_${calibration_loss_wt}_subset_${subset_percent}_prec_${precision}  \
-          trainer.limit_train_batches=2 \
-          trainer.limit_val_batches=2 \
-          trainer.limit_test_batches=2 \
-          trainer.check_val_every_n_epoch=1
-
+          expt_suffix=edge_blur_sigma_${blur_sigma}_subset_${subset_percent}_prec_${precision}
         done
       done
     done
   done
 done
-#
-#  \
-#          trainer.limit_train_batches=2 \
-#          trainer.limit_val_batches=2 \
-#          trainer.limit_test_batches=2 \
+
+
+# \
+#          trainer.limit_train_batches=1 \
+#          trainer.limit_val_batches=1 \
+#          trainer.limit_test_batches=1 \
 #          trainer.check_val_every_n_epoch=1
